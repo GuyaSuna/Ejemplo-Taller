fd7fdaa779c910a3886612d7aa8885a556078d9a
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2026-02-04 20:10:44 -0300

    agregados detalles

--- src/App.js
+++ src/App.js
@@ -1,33 +1,152 @@
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'react-bootstrap';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
+
+import "./App.css";
 
 function App() {
+  const [usuario, setUsuario] = useState("");
+  const [password, setPassword] = useState("");
+  const [mensaje, setMensaje] = useState("");
+
+
+  const personas = [
+    { id: 1, nombre: "Martín", edad: 15 },
+    { id: 2, nombre: "Sofía", edad: 22 },
+    { id: 3, nombre: "Andrés", edad: 24 },
+  ];
+
+  const clases = [
+    {id: 1 , cantidadSillas: 25},
+    {id: 2 , cantidadSillas: 30},
+    {id: 3 , cantidadSillas: 20}
+  ]
+
+  const claseSillasPar = clases.find((clase) => clase.cantidadSillas % 2 === 0 ); 
+
+  const clasesSillasPar = clases.filter((clase) => clase.cantidadSillas % 2 === 0);
+
+
+
+
+
+
+  const numeroPar = personas.find((per) => per.edad % 2 === 0);
+
+  const edadesFiltradas = personas.filter((per) => per.edad % 2 === 0);
+
+  const edadesTotal = personas.reduce((suma, item) => suma + item.edad, 0);
+
+
+
+
+
+
+
+
+const numeros = [1,2,3]
+
+const suma = numeros.reduce((suma , item) => suma + item , 0);
+
+const dobles2 = numeros.map((num) => num * 2 );
+
+
+
+
+
+
+
+
+
+
+
+
+  const dobles = personas.map((per) => (
+    <p key={per.id}>
+      !Hola {per.nombre} , tu edad es de: {per.edad}.
+    </p>
+  ));
+
+  console.log(dobles);
+
+
+
+  const testTaller = (nombre = "Invitado") => console.log(nombre) 
+
+
+
+
+
+  const manejarLogin = () => {
+    if (usuario === "" || password === "") {
+      setMensaje("Tenés que completar todos los campos");
+    } else {
+      setMensaje("Presionaste el botón de login");
+    }
+  };
 
   return (
     <>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"  /> 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contenedor">
+        <Card className="text-center">
+          <Card.Header>Login Muestra Taller</Card.Header>
+          <Card.Body>
+            <input
+              type="text"
+              placeholder="Usuario"
+              value={usuario}
+              onChange={(e) => setUsuario(e.target.value)}
+              className="input"
+            />
 
-    </div>
-    <Button></Button>
-     </>
+            <input
+              type="password"
+              placeholder="Contraseña"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input"
+            />
+            <Button variant="primary" onClick={manejarLogin}>
+              Login
+            </Button>
+          </Card.Body>
+          <Card.Footer className="text-muted">{mensaje}</Card.Footer>
+        </Card>
+      </div> 
+
+
+
+
+
+
+      {dobles2}
+
+        <br></br>
+
+
+
+      {claseSillasPar.cantidadSillas}
+
+
+      <br></br>
+
+
+        {clasesSillasPar.map((clase) => <div>
+          <h1> {clase.id} </h1>
+          <h2> {clase.cantidadSillas}  </h2>
+        </div>)}
+
+  <br></br>
+
+{suma}
+
+
+
+
+
+    </>
   );
 }
-
 
 export default App;
